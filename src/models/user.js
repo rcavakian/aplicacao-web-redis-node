2c130961347bfd950450193f080b88a5b2841458
Ricardo Avakian <rcavakian@gmail.com>
2024-08-30 13:50:18 -0300

    Adicionar novas funcionalidades no /services/user1

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,9 @@
 // TODO: Criar estrutura de dados no js para utilizar o hash no Redis
-import redisClient from '../database/database';
 import { v4 as uuidv4 } from 'uuid'; // para gerar id unico
 
 class User {
     constructor(login, name, email, active) {
-        this.userid =  `user:${uuidv4()}`;
+        this.userId =  `user:${uuidv4()}`;
         this.login = login;
         this.name = name;
         this.email = email;
--- src/services/user.js
+++ src/services/user.js
@@ -1,18 +1,17 @@
-import redisClient from "../database/database";
+const redisClient = require ('../database/database');
 const usersSetService = require ('./usersSet');
 import { v4 as uuidv4 } from 'uuid'; // para gerar id unico
-import User from '../models/user';
+const User = require('../models/user');
 
 
 /**
  * Função que cria um novo usuário e insere no Redis
- * @param {*} redisClient 
  * @param {string} login 
  * @param {string} name 
  * @param {string} email 
  * @param {boolean} active ativo (1) ou inativo (0)
  */
-async function addUser(redisClient, User) {
+async function addUser(User) {
     // Registrar o momento em que o usuário foi criado
     const timeStamp = Date.now();
 
@@ -20,32 +19,54 @@ async function addUser(redisClient, User) {
     await redisClient.hSet(`${User.userId}`, 'login', User.login, 'name', User.name, 'email', User.email, 'active', User.active);
 
     // Incluir novo usuário no Sorted Set usersSet
-    usersSetService.addUserToSet(redisClient, User.userId, timeStamp);    
+    usersSetService.addUserToSet(User.userId, timeStamp);    
 }
 
 /**
  * Função que retorna um usuário
- * @param {*} redisClient 
  * @param {string} userId 
  */
-async function getUser(redisClient, userId) {
-    return await redisClient.hGetAll(`user:${userId}`);
+async function getUser(User) {
+    return await redisClient.hGetAll(`${User.userId}`);
 }
 
 /**
  * Função que desativa um usuário
  * 
- * @param {*} redisClient 
  * @param {*} userId 
  * @param {boolean} active ativo (1) ou inativo (0)
  */
-async function disableUser(redisClient, userId, active) {
-    await redisClient.hSet(`user:${userId}`, 'active', active);
+async function disableUser(userId, active) {
+    const userFound = await redisClient.hGetAll(`${userId}`);
+
+    if (userFound) {
+        userFound.active = active;
+        await redisClient.hSet(`${userId}`, 'active', active)
+    }
+    return userFound;
 }
 
-// Função que atualiza um usuário
-function updateUser (redisClient, userId, login, name, email, active) {
-
+/**
+ * Função que atualiza um usuário
+ * @param {*} redisClient 
+ * @param {*} userId 
+ * @param {*} data 
+ * @returns 
+ */
+async function updateUser (redisClient, userId, data) {
+    const userFound = await redisClient.hGetAll(`${userId}`);
+    // TODO: verificar forma melhor de checar se usuraio existe ou não e comparar o que é para de fato atualizar
+    // consultar https://redis.io/docs/latest/commands/hgetall/
+    if (userFound) {
+        userFound.login = data.login ?? userFound.login;
+        userFound.name = data.name ?? userFound.name;
+        userFound.email = data.email ?? userFound.email;
+        userFound.active = data.active ?? userFound.active;
+        await redisClient.hSet(`${userId}`, 'login', userFound.login, 'name', userFound.name, 'email', userFound.email, 'active', userFound.active)
+    } else {
+        // TODO implementar aqui ou no controller erro por não ter localizado o usuario
+    }
+    return userFound;
 }
 
-module.exports = { addUser, getUser, getAllUsers, deleteUser, updateUser };
+module.exports = { addUser, getUser, disableUser, updateUser };
