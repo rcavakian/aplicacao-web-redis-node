836a11863a77fe64440858a7dcbf7a4554bab155
Ricardo Avakian <rcavakian@gmail.com>
2024-09-03 11:16:11 -0300

    Adicionar function updateUserFromSet() em /controllers/usersSet

--- src/controllers/usersSet.js
+++ src/controllers/usersSet.js
@@ -7,16 +7,41 @@ function listAllUsers(req, res) {
         })
 }
 
-function addUsertoSet() {
-
+function addUsertoSet(req, res) {
+  service.addUsertoSet(req.body).then(
+    (newUser) => {
+      return res.status(201).send({
+        message: "Novo usuário adicionado",
+        user: newUser,
+      });
+    },
+    (error) => {
+      return res.status(500).send({
+        message: error,
+      });
+    }
+  );
 }
 
 function removeUserFromSet() {
 
 }
 
-function updateUserFromSet() {
-
+function updateUserFromSet(req, res) {
+    service.updateUserFromSet(req.params.id).then(
+        (userUpdated) => {
+            if(!userUpdated) {
+                return res.status(404).send({ message: "Usuário não encontrado"})
+            }
+            return res.status(201).send({
+                message: 'Usuario editado com sucesso',
+                user: userUpdated,
+            });
+        },
+        (error) => {
+            return res.status(500).send({ message: "Usuário não encontrado"});
+        }
+    );
 }
 
 function listUserFromSet() {
